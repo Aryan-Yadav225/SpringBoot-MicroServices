ec7d828cc0a8133bd343bd7eca49d5b5dd9f4f20
Aryan Yadav <aryan225code@gmail.com>
2025-08-29 15:21:55 +0530

    Adding Sync Communication using FeignClient and wrote TestCases

--- order-service/src/test/java/com/aryan/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/aryan/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aryan.microservices.order;
 
+import com.aryan.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
@@ -14,6 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
     @ServiceConnection
@@ -43,6 +46,7 @@ class OrderServiceApplicationTests {
                 }
                 """;
 
+        InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
@@ -57,5 +61,26 @@ class OrderServiceApplicationTests {
 
         assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
+
+    @Test
+    void shouldFailOrderWhenProductIsNotInStock() {
+        String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1000
+                }
+                """;
+        InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(500);
+    }
 
 }
